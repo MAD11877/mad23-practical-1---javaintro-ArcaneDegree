9a1e759e054c3938c6821c5257fe4a1bdf3feb00
ArcaneDegree <s10239861@connect.np.edu.sg>
2023-06-30 22:45:20 +0800

    Changelog: - Modified implementation of solution for Question 3.

--- Question3.java
+++ Question3.java
@@ -6,12 +6,10 @@ public class Question3
   {
     Scanner in = new Scanner(System.in);
 
-    System.out.print("Enter an integer: ");
-
     int integer = in.nextInt();
 
     int result = integer * integer;
 
-    System.out.println("Result: " + result);
+    System.out.println(result);
   }
 }
